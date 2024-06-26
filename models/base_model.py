204ac97af3532b02ad42f2d7c7120bfd4a54f7b0
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2024-06-26 15:54:14 +0300

    Fixed some bugs in the files

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
+from models import storage
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DateTime
@@ -11,13 +12,12 @@ Base = declarative_base()
 class BaseModel:
     """A base class for all hbnb models"""
     id = Column(String(60), primary_key=True, nullable=False)
-    created_at = Column(DateTime, default=datetime.now(), nullable=False)
-    updated_at = Column(DateTime, default=datetime.now(), nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
 
     def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
         if not kwargs:
-            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.utcnow()
             self.updated_at = datetime.utcnow()
@@ -41,7 +41,6 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
         self.updated_at = datetime.utcnow()
         storage.new(self)
         storage.save()
@@ -60,5 +59,4 @@ class BaseModel:
 
     def delete(self):
         """Delete the current instance from the storage"""
-        from models import storage
         storage.delete(self)
--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -19,9 +19,9 @@ class DBStorage:
         HBNB_MYSQL_DB = os.getenv('HBNB_MYSQL_DB')
         HBNB_ENV = os.getenv('HBNB_ENV')
 
-        self.__engine = create_engine(f'''
-                                      mysql+mysqldb://{HBNB_MYSQL_USER}:
-                                      {HBNB_MYSQL_PWD}@{HBNB_MYSQL_HOST}/
+        self.__engine = create_engine(f'''\
+                                      mysql+mysqldb://{HBNB_MYSQL_USER}:\
+                                      {HBNB_MYSQL_PWD}@{HBNB_MYSQL_HOST}/\
                                       {HBNB_MYSQL_DB}''',
                                       pool_pre_ping=True)
 
